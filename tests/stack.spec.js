d0ec44535c88437915285fcb10ef7e2bae14959d
satyr- <oleksii.tsykunov@gmail.com>
2019-06-03 17:37:51 +0300

    Implement additional test cases. Add validation routine for a stack input value.

--- tests/stack.spec.js
+++ tests/stack.spec.js
@@ -15,12 +15,16 @@ describe('stack', () => {
 		stack = new Stack();
 	});
 
-	describe('initStorage', () => {
-		test('should init storage', async () => {
-			const STACK = 'stack';
-			const logging = process.env.NODE_ENV === 'development' || false;
-			const ttl = true; // default flag, equals 24h TTL
+	afterEach(() => {
+		jest.resetAllMocks();
+	});
 
+	describe('initStorage', () => {
+		const STACK = 'stack';
+		const logging = false;
+		const ttl = true;
+
+		test('should init stack with default value', async () => {
 			await stack.initStorage();
 
 			expect(nodePersistMock.init).toHaveBeenCalledWith({logging, ttl});
@@ -28,9 +32,101 @@ describe('stack', () => {
 			expect(stack._stack).toEqual([]);
 			expect(stack.isStorageInitialized).toEqual(true);
 		});
+
+		test('should init stack with value of the local storage', async () => {
+			const storageValue = '{"key":"stack","value":"[]"}';
+
+			nodePersistMock.getItem = jest.fn().mockImplementation(() => storageValue);
+
+			await stack.initStorage();
+
+			expect(nodePersistMock.init).toHaveBeenCalledWith({logging, ttl});
+			expect(nodePersistMock.getItem).toHaveBeenCalledWith(STACK);
+			expect(stack._stack).toEqual(JSON.parse(storageValue));
+			expect(stack.isStorageInitialized).toEqual(true);
+		});
 	});
-	describe('isStorageInitialized', () => {});
-	describe('length', () => {});
-	describe('pop', () => {});
-	describe('push', () => {});
+
+	describe('isStorageInitialized', () => {
+		test('should return default storage state', () => {
+			expect(stack.isStorageInitialized).toEqual(false);
+		});
+
+		test('should return initiated storage state', async () => {
+			await stack.initStorage();
+
+			expect(stack.isStorageInitialized).toEqual(true);
+		});
+	});
+
+	describe('length', () => {
+		test('should return default stack length', () => {
+			let length = stack.length();
+
+			expect(length).toEqual(0);
+		});
+
+		test('should return current stack length', async () => {
+			await stack.initStorage();
+			await stack.push('some_data');
+
+			let length = stack.length();
+
+			expect(length).toEqual(1);
+		});
+	});
+
+	describe('pop', () => {
+		test('should eject last element from the stack', async () => {
+			const data = 'some_data';
+
+			await stack.initStorage();
+			await stack.push(data);
+
+			expect(await stack.pop()).toEqual(data);
+			expect(stack.length()).toEqual(0);
+		});
+
+		test('should return undefined value if stack is empty', async () => {
+			await stack.initStorage();
+
+			expect(await stack.pop()).toBeUndefined();
+			expect(stack.length()).toEqual(0);
+		});
+
+		test('should do nothing if storage was not initialized', async () => {
+			expect(stack.isStorageInitialized).toEqual(false);
+			expect(await stack.pop()).toBeUndefined();
+		});
+	});
+
+	describe('push', () => {
+		test('should push non empty value and return true', async () => {
+			await stack.initStorage();
+
+			expect(await stack.push('some_value')).toEqual(true);
+			expect(await stack.push(false)).toEqual(true);
+			expect(await stack.push(0)).toEqual(true);
+			expect(await stack.push(1)).toEqual(true);
+			expect(await stack.push(-1)).toEqual(true);
+			expect(await stack.push([1])).toEqual(true);
+			expect(await stack.push({'key':'value'})).toEqual(true);
+		});
+
+		test('should push falsy value and return false', async () => {
+			await stack.initStorage();
+
+			expect(await stack.push('')).toEqual(false);
+			expect(await stack.push(undefined)).toEqual(false);
+			expect(await stack.push(null)).toEqual(false);
+			expect(await stack.push(NaN)).toEqual(false);
+			expect(await stack.push([])).toEqual(false);
+			expect(await stack.push({})).toEqual(false);
+		});
+
+		test('should do nothing if storage was not initialized', async () => {
+			expect(stack.isStorageInitialized).toEqual(false);
+			expect(await stack.push('value')).toBeUndefined();
+		});
+	});
 });
--- tests/ttl-store.spec.js
+++ tests/ttl-store.spec.js
@@ -28,8 +28,144 @@ describe('ttl-store', () => {
 		});
 	});
 
-	describe('isStorageInitialized', () => {});
-	describe('get', () => {});
-	describe('set', () => {});
-	describe('delete', () => {});
+
+	describe('isStorageInitialized', () => {
+		test('should return default storage state', () => {
+			expect(ttlStore.isStorageInitialized).toEqual(false);
+		});
+
+		test('should return initiated storage state', async () => {
+			await ttlStore.initStorage();
+
+			expect(ttlStore.isStorageInitialized).toEqual(true);
+		});
+	});
+
+	describe('get', () => {
+		const key = 'key';
+		const value = 'some_value';
+		const storageValue = {[key]: value};
+
+		nodePersistMock.getItem = jest.fn().mockImplementation((key) => storageValue[key]);
+
+		test('should get value by the key', async () => {
+			await ttlStore.initStorage();
+
+			expect(await ttlStore.get(key)).toEqual(value);
+			expect(nodePersistMock.getItem).toHaveBeenCalledWith(key);
+		});
+
+		test('should return undefined if there are no key was found', async () => {
+			const non_existing_key = 'other_key';
+
+			await ttlStore.initStorage();
+
+			expect(await ttlStore.get(non_existing_key)).toBeUndefined();
+			expect(nodePersistMock.getItem).toHaveBeenCalledWith(non_existing_key);
+		});
+
+		test('should return false if there are no key was provided', async () => {
+			await ttlStore.initStorage();
+
+			expect(await ttlStore.get()).toEqual(false);
+			expect(nodePersistMock.getItem).not.toHaveBeenCalled();
+		});
+
+		test('should do nothing if storage was not initialized', async () => {
+			expect(ttlStore.isStorageInitialized).toEqual(false);
+			expect(await ttlStore.get('key')).toBeUndefined();
+		});
+	});
+
+	describe('set', () => {
+		const key = 'key';
+		const value = 'some_value';
+		const default_ttl = null;
+
+		nodePersistMock.setItem = jest.fn().mockImplementation((key, value, options) => {
+			return {key, value, ttl: options.ttl};
+		});
+
+		test('should set value by the key with default ttl and return saved object', async () => {
+			const storageValue = {key, value, ttl: default_ttl};
+			const options = {ttl: default_ttl};
+
+			await ttlStore.initStorage();
+
+			expect(await ttlStore.set(key, value)).toEqual(storageValue);
+			expect(nodePersistMock.setItem).toHaveBeenCalledWith(key, value, options);
+		});
+
+		test('should set value by the key with specified ttl and return saved object', async () => {
+			const ttl = 1000;
+			const storageValue = {key, value, ttl};
+			const options = {ttl: ttl};
+
+			await ttlStore.initStorage();
+
+			expect(await ttlStore.set(key, value, ttl)).toEqual(storageValue);
+			expect(nodePersistMock.setItem).toHaveBeenCalledWith(key, value, options);
+		});
+
+		test('should return false if there are no key or value were provided', async () => {
+			await ttlStore.initStorage();
+
+			expect(await ttlStore.set(key)).toEqual(false);
+			expect(nodePersistMock.getItem).not.toHaveBeenCalled();
+
+			expect(await ttlStore.set('', value)).toEqual(false);
+			expect(nodePersistMock.getItem).not.toHaveBeenCalled();
+
+			expect(await ttlStore.set()).toEqual(false);
+			expect(nodePersistMock.getItem).not.toHaveBeenCalled();
+		});
+
+		test('should do nothing if storage was not initialized', async () => {
+			expect(ttlStore.isStorageInitialized).toEqual(false);
+			expect(await ttlStore.set('key', 'value')).toBeUndefined();
+		});
+	});
+
+	describe('delete', () => {
+		const key = 'key';
+		const value = 'some_value';
+		const storageValue = {[key]: value};
+		const result_success = {removed: true, existed: true, file: 'some_path_to_file'};
+		const result_fail = {removed: false, existed: false, ...result_success};
+
+		nodePersistMock.removeItem = jest.fn().mockImplementation((key) => {
+			if (storageValue[key])
+				return result_success;
+			else
+				return result_fail;
+		});
+
+		test('should remove value by the key and return result', async () => {
+			await ttlStore.initStorage();
+
+			expect(await ttlStore.delete(key)).toEqual(result_success);
+			expect(nodePersistMock.removeItem).toHaveBeenCalledWith(key);
+		});
+
+		test('should fail to remove value by the key if there is no key was found and return result', async () => {
+			const non_existing_key = 'other_key';
+
+			await ttlStore.initStorage();
+
+			expect(await ttlStore.delete(non_existing_key)).toEqual(result_fail);
+			expect(nodePersistMock.removeItem).toHaveBeenCalledWith(non_existing_key);
+		});
+
+		test('should return false if there are no key was provided', async () => {
+			await ttlStore.initStorage();
+
+			expect(await ttlStore.delete()).toEqual(false);
+			expect(nodePersistMock.removeItem).not.toHaveBeenCalled();
+		});
+
+		test('should do nothing if storage was not initialized', async () => {
+			expect(ttlStore.isStorageInitialized).toEqual(false);
+			expect(await ttlStore.delete('key')).toBeUndefined();
+		});
+	});
 });
